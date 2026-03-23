e73468db87813060f4e736eba8188f1b47e7f420
juliolmuller <juliolmuller@outlook.com>
2026-03-23 17:11:11 -0300

    feat(cnpj-dv): define constants for package

---
+++ src/cnpj-dv.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lacus\BrUtils\Cnpj;
+
+/**
+ * Minimum number of characters required for the CNPJ check digits calculation.
+ */
+const CNPJ_MIN_LENGTH = 12;
+
+/**
+ * Maximum number of characters accepted as input for the CNPJ check digits calculation.
+ */
+const CNPJ_MAX_LENGTH = 14;
